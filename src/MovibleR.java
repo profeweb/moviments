4df2cd0a01e86280ebe63b016377bf1f7e8d8154
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2024-05-14 12:09:20 +0200

    initial

---
+++ src/MovibleR.java
@@ -0,0 +1,37 @@
+import processing.core.PApplet;
+
+import static java.lang.Math.random;
+
+public class MovibleR extends Movible{
+
+    float xDir, yDir;
+
+    public MovibleR(float x, float y, float xd, float yd) {
+        super(x, y);
+        this.xDir = xd;
+        this.yDir = yd;
+    }
+
+    public void move(int w, int h){
+        x += xDir * 5;
+        y += yDir * 5;
+
+        if(x<=25 || x>=w-25){
+            xDir*= -1;
+        }
+        if(y<=25 || y>=h-25){
+            yDir*= -1;
+        }
+    }
+
+    public void colisio(MovibleR altre){
+        float d = PApplet.dist(this.x, this.y, altre.x, altre.y);
+        if(d<=50){
+            this.xDir*=-1;
+            this.yDir*=-1;
+
+            altre.xDir*=-1;
+            altre.yDir*=-1;
+        }
+    }
+}
--- src/Moviments.java
+++ src/Moviments.java
@@ -3,11 +3,14 @@ import processing.core.PApplet;
 public class Moviments extends PApplet {
 
     // Objectes de classe Movible
-    Movible mh, mv, mc, me, ms, mo;
+    Movible mh, mv, mc, me, ms, mo, mr, mr2;
+
+    int numRs = 5;
+    MovibleR[] mrs;
 
     // Configuració bàsica de l'aplicació
     public void settings(){
-        size(800, 800);
+        size(800, 400);
         smooth(10);
     }
 
@@ -24,6 +27,16 @@ public class Moviments extends PApplet {
         me = new MovibleE(0, 0, width/2, height/2, 200, 100);
         ms = new MovibleS(0, 0, width/2, height/2, 0);
         mo = new MovibleO(0, 0, 0, 0, height/2);
+
+        mrs = new MovibleR[numRs];
+
+        for(int i=0; i<numRs; i++) {
+            float x = random(100, width-100);
+            float y = random(100, height-100);
+            float dx = random(-1, 1);
+            float dy = random(-1, 1);
+            mrs[i] = new MovibleR(x, y, dx, dy);
+        }
     }
 
     // Bucle (30 vegades per segon)
@@ -48,11 +61,24 @@ public class Moviments extends PApplet {
         ms.display(this);
         ms.move(0.05f);
 
-         */
-
         mo.display(this);
         mo.move(this.width);
 
+         */
+
+        for(int i=0; i<numRs; i++){
+            mrs[i].display(this);
+            mrs[i].move(width, height);
+            for(int j=i+1; j<numRs; j++){
+                mrs[i].colisio(mrs[j]);
+            }
+        }
+    }
+
+    public void mousePressed(){
+        float dx = random(-1, 1);
+        float dy = random(-1, 1);
+        mr = new MovibleR(width/2, height/2, dx, dy);
     }
 
     public void keyPressed(){
